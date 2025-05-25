0790ba5788f86b81faacc64858cacf8bc650886e
Bernard <22891809+bernardzaborniak@users.noreply.github.com>
2025-05-25 14:08:30 +0200

    Cleaned up some files

---
+++ Assets/Scripts/FurnitureManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FurnitureManager : MonoBehaviour
+{
+    // TODO COnvert furniture from MRUK scanned model from oculus into our own
+    // 
+    // Has Method PullFurniture, ConvertFurniture, SpawnFurniture (Instantiate/Destroy)
+
+    // maybe use List of List to save rooms,
+    // add aditional furniture object data model script
+    // non-monobehaviour for saving
+    // monobehaviour for displaying
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
