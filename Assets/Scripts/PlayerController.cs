e7958ee1ffb278d2a012a7d623f9f877f0cd3c08
nikulind <38283025+nikulind@users.noreply.github.com>
2025-06-04 16:39:47 +0200

    cleanup in playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,26 +25,19 @@ public class PlayerController : MonoBehaviour
 
     void switchScene()
     {
-        // "Disabling destroys objects that are within the scope of the respective components, such as layers and projection meshes, which must be recreated when re-enabled."
-        // -> roomscan within scope? save objects before and load after switch necessary?
-        // are both directions covered?
+        // change from AR to VR (ADD SKYBOX!!)
         if (ovrPassthroughLayer.enabled)
         {
             roomsManager.SaveRoomScanFromVisualization();
-            //Debug.Log("Saved");
             ovrPassthroughLayer.enabled = !ovrPassthroughLayer.enabled;
-            //Debug.Log("Switched");
             roomsManager.ShowRoomVariation(0);
-            //Debug.Log("Loaded");
         }
+        // change from VR to AR
         else
         {
             roomsManager.SaveRoomVariationFromVisualization(0);
-            //Debug.Log("Saved");
             ovrPassthroughLayer.enabled = !ovrPassthroughLayer.enabled;
-            //Debug.Log("Switched");
             roomsManager.ShowRoomScan();
-            //Debug.Log("Loaded");
         }
     }
 }
