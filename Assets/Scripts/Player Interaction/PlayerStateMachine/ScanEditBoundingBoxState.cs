eb9d7a9a6acc2c53ca861c7eb7d45cca868a0d7f
Bernard <22891809+bernardzaborniak@users.noreply.github.com>
2025-06-16 00:08:50 +0200

    added empty states as preperation for advanced scan edit mode

---
+++ Assets/Scripts/Player Interaction/PlayerStateMachine/ScanEditBoundingBoxState.cs
@@ -0,0 +1,19 @@
+public class ScanEditBoundingBoxState : PlayerControllerInteractionState
+{
+    // this state is still optional, it would allow us to edit bounding boxes by moving faces like in blender
+
+    public override void OnStateEnter()
+    {
+
+    }
+
+    public override void OnStateExit()
+    {
+        
+    }
+
+    public override void UpdateState()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/Player Interaction/PlayerStateMachine/ScanRegisterNewFurnitureState.cs
@@ -0,0 +1,22 @@
+public class ScanRegisterNewFurnitureState : PlayerControllerInteractionState
+{
+    // tis state is similar to what meta has, it allows to create new bounding boxes with our controllers
+
+    //maybe have a small menu on the left open that allows us to switch between create new furniture and add walls mode 
+    // add walls is disabled in one test instance?
+
+    public override void OnStateEnter()
+    {
+
+    }
+
+    public override void OnStateExit()
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+
+    }
+}
